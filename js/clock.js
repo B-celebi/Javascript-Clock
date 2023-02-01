370068967cd67a7042d9779fb4dbef19ccacfe29
Batuhan <batuhancelebi.business@gmail.com>
2023-02-01 14:55:03 +0300

    This is a simple Javascript Clock project for www.patika.dev's homework.

---
+++ js/clock.js
@@ -0,0 +1,59 @@
+let input = prompt("Adınız nedir?");
+
+//Eğer Girilen İsim String Değilse Hata Verecek Bir If.
+if (Number.isNaN(parseInt(input))) {
+    let myname = document.querySelector("span#myName");
+
+    //Eğer İsim Boş Bırakılırsa :)
+    if (input == "") {
+        myname.textContent = "İsimsiz :)"
+    } else {
+        myname.textContent = input;
+    }
+
+    const clock = document.querySelector("div#myClock");
+
+    //Saat Fonksiyonu
+    function saatGuncel() {
+        const date = new Date();
+        let gun = date.getDay();
+        const saat = date.getHours();
+        const dakika = date.getMinutes();
+        const saniye = date.getSeconds();
+        switch (gun) {
+            case 0:
+                gun = "Pazar";
+                break;
+            case 1:
+                gun = "Pazartesi";
+                break;
+            case 2:
+                gun = "Salı";
+                break;
+            case 3:
+                gun = "Çarşamba";
+                break;
+            case 4:
+                gun = "Perşembe";
+                break;
+            case 5:
+                gun = "Cuma";
+                break;
+            case 6:
+                gun = "Cumartesi";
+                break;
+        }
+        // Saat Bilgisi İçerik Değiştirme
+        clock.innerHTML = `${saat}:${dakika}:${saniye} ${gun}`;
+    }
+
+    //Saatin Sürekli Güncellenmesi İçin setInterval Metodu Kullanımı.
+    setInterval(saatGuncel(), 1000);
+
+}
+else {
+    alert("Lütfen isminizi doğru giriniz");
+    document.body.classList.add("hata");
+    document.body.innerHTML = "<h1>HATA</h1>";
+}
+
